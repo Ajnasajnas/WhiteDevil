4263fd78505ae5d335c1ab99da9e0ce16c0772ba
ElsaMwol <87955538+Ajnasajnas@users.noreply.github.com>
2021-09-18 08:04:57 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     
     
    
-  r_text[0] = "https://telegra.ph/file/ad4d2ee49c2fc94dfddaa.jpg";
+  r_text[0] = "";
     
     
     var i = Math.floor(1*Math.random())
